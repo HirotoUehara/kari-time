a0ef27e06182f45d78a86642b254ae08a8a48139
Ririka Kinjo <rilly0523@gmail.com>
2022-08-19 21:14:15 +0900

    itemページ(仮)

--- app.py
+++ app.py
@@ -10,16 +10,12 @@ app.secret_key = "kari-time"
 
 @app.route("/")
 def template():
-    return render_template("top.html")
-
-
-
-
+    return render_template("base.html")
 
 
 
 @app.route("/item_list" , methods = ["GET"])
-def item():
+def item_list():
   # DBへの接続と、データを全部取ってくる
   #conn = sqlite3に接続して
   conn = sqlite3.connect("Item.db")
@@ -34,6 +30,27 @@ def item():
   c.close
   print(item_list)
   return render_template("item_list.html",item_list = item_list)
+
+
+
+@app.route("/item" , methods = ["GET"])
+def item():
+  # DBへの接続と、データを全部取ってくる
+  #conn = sqlite3に接続して
+  conn = sqlite3.connect("Item.db")
+  #DBを操作できるようにして
+  c = conn.cursor()
+  # ()内のSQL文を実行
+  c.execute("SELECT item_name, price, abv, type, explain FROM Item where id = 1;")
+  # タスクのデータを入れる配列を定義
+  item = []  #配列の初期化
+  for row in c.fetchall(): #row=新しく作った変数
+      item.append({"item":row[0],"price":row[1],"abv":row[2],"type":row[3],"explain":row[4]})
+  c.close
+  print(item)
+  return render_template("item.html", item = item)
+
+
 
 
 
